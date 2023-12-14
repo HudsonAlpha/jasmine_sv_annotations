df69dd3f19b98ee4b759fea606e26ed2da6bbbb5
James Lawlor <jlawlor@hudsonalpha.org>
2023-12-14 00:26:23 -0600

    All annotations transfer option

--- scripts/build_annotation_table.py
+++ scripts/build_annotation_table.py
@@ -2,32 +2,40 @@ from lmdbm import Lmdb
 from cyvcf2 import VCF
 import os, shutil
 import pprint
+import json
+from itertools import islice
+
+def batched(iterable, n):
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    # Code from rough equivalent of itertools.batched https://docs.python.org/3/library/itertools.html#itertools.batched
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
 
 # snakemake object is inherited
 dbfile = os.path.dirname(snakemake.output[0])
 print(dbfile)
-try:
-    shutil.rmtree(dbfile)
-except FileNotFoundError:
-    pass
 
-
-with Lmdb.open(dbfile, 'c') as db:
-    # db['test_record'] = 'test_record'
-    # print(db['test_record'])
-    # print('done test record')
-    for annotation in snakemake.params.annotations:
-        print(annotation)
-        vcf = VCF(annotation['vcf'])
+with Lmdb.open(file=dbfile, flag='n', map_size=1024^3) as db:
+    for annotationsource in snakemake.params.annotations:
+        print(annotationsource)
+        vcf = VCF(annotationsource['vcf'])
         # add an item to the db that is {description}_{annotation} = serialized vcf header info for that annotation so we can look this up to modify header of output vcf later
-        #print(vcf)
-        #db.update({v.ID: "test" for v in vcf})
-        db.update(
-            {
-                v.ID: ";".join(
-                    (f'{annotation["description"]}_{k}={v.INFO.get(k,".")}' for k in annotation['annotations'])
-                ) for v in vcf
-            }
-        )
+        if annotationsource['annotations'] == '*':
+            annotationsource['annotations'] = [x['ID'] for x in vcf.header_iter() if x['HeaderType'] == 'INFO']
+            print(annotationsource['annotations'])
+        for field in annotationsource['annotations']:
+            db[f'{annotationsource["description"]}_{field}'] = json.dumps(vcf.get_header_type(field))
+        for vbatch in batched(vcf, 10000):
+            #db[v.ID] = json.dumps({f'{annotationsource["description"]}_{field}': v.INFO.get(field,".") for field in annotationsource['annotations']})
+            db.update(
+                {
+                    v.ID: json.dumps(
+                        {f'{annotationsource["description"]}_{field}': v.INFO.get(field,".") for field in annotationsource['annotations']}
+                        ) for v in vbatch
+                }
+            )
     #     #data = {v.ID: ";".join((f'{annotation["description"]}_{k}={v.INFO.get(k,".")}' for k in annotation['annotations'])) for v in vcf}
     #     #print(data)
--- scripts/transfer_annotations.py
+++ scripts/transfer_annotations.py
@@ -2,16 +2,30 @@ from cyvcf2 import VCF, Writer
 from lmdbm import Lmdb
 import os
 import json
+from time import sleep
+from lmdb import InvalidParameterError
 
 # inherit snakemake object
 dbfile = os.path.dirname(snakemake.input['annotation_db'])
 print(dbfile)
-annotation_db = Lmdb.open(dbfile, 'r')
+attempts = 0
+while attempts < 10:
+    try:
+        annotation_db = Lmdb.open(dbfile, 'r')
+        break
+    except InvalidParameterError:
+        print(f'Waiting for db to unlock... attempt {attempts}')
+        attempts += 1
+        sleep(10)
+
 input_vcf_path = snakemake.input['vcf']
 output_vcf_path = snakemake.output['vcf']
 
 input_vcf = VCF(input_vcf_path)
 for annotationsource in snakemake.params.annotations:
+    if annotationsource['annotations'] == '*':
+        check_vcf = VCF(annotationsource['vcf'])
+        annotationsource['annotations'] = [x['ID'] for x in check_vcf.header_iter() if x['HeaderType'] == 'INFO']
     for annotation in annotationsource['annotations']:
         annokey = f'{annotationsource["description"]}_{annotation}'
         annoinfo = json.loads(annotation_db[annokey])
@@ -29,9 +43,12 @@ for variant in input_vcf:
                 annos_to_add = json.loads(annotation_db[fk])
             except KeyError:
                 annos_to_add = {}
-            print(f'{annos_to_add} to {variant.CHROM} {variant.POS} {variant.REF} {variant.ALT}')
+            #print(f'{annos_to_add} to {variant.CHROM} {variant.POS} {variant.REF} {variant.ALT}')
             for k,v in annos_to_add.items():
-                variant.INFO[k] = v
+                try:
+                    variant.INFO[k] = v
+                except AttributeError: # attempt to handle strangely formatted INFO fields (tuples,etc.)
+                    variant.INFO[k] = str(k)
         output_vcf.write_record(variant)
 output_vcf.close()
 
